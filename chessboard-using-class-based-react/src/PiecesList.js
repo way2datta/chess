9d7733ce63b36577d1bb2f7a8173e75cf9c0c947
Dagttatray Kale <way2datta@gmail.com>
2019-12-14 16:39:33 +0530

    Piece list and chessboard

---
+++ chessboard-using-class-based-react/src/PiecesList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PiecesList extends Component {
+    render() {
+        return (
+            <div>
+                Select a piece
+            </div>
+        )
+    }
+}
