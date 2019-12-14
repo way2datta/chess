fb815042626c3ab51a924887897c2249faa9f6c4
Dagttatray Kale <way2datta@gmail.com>
2019-12-14 16:44:00 +0530

    Introduced piece provider

--- chessboard-using-class-based-react/src/PiecesList.js
+++ chessboard-using-class-based-react/src/PiecesList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { getPieces } from "./PieceProvider";
 
 export default class PiecesList extends Component {
     render() {
+        const pieces = getPieces();
         return (
-            <div>
-                Select a piece
-            </div>
+            <ul>
+                {
+                    pieces.map(function (piece) {
+                        return <li> {piece.name}</li>
+                    })
+                }
+            </ul>
         )
     }
 }
