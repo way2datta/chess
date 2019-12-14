2a447cc20ba8144fb60075fb2621c6480931413e
Dagttatray Kale <way2datta@gmail.com>
2019-12-14 19:09:25 +0530

    Turned on prettier and black and white cells highlighted

--- chessboard-using-class-based-react/src/ChessboardView.js
+++ chessboard-using-class-based-react/src/ChessboardView.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import Chessboard from './Chessboard';
 
 export default class ChessboardView extends Component {
-  static renderEmptyBoard() {
-    return Chessboard.fileTitles.map((fileTitle) => (
-      <div key={fileTitle} className="row">
+  getAdditionalCssClasses(fileIndex, rankIndex) {
+    if (fileIndex % 2 === 0) {
+      return rankIndex % 2 === 0 ? "white" : "black";
+    }
+    return rankIndex % 2 === 0 ? "black" : "white";
+  }
+  renderEmptyBoard() {
+    return Chessboard.fileTitles.map((fileTitle, fileIndex) => (
+      <div key={fileTitle} className='row'>
         {
-          Chessboard.rankTitles.map((rankTitle) => <div key={rankTitle} className="cell">{`${`${rankTitle}${fileTitle}`}`}</div>)
+          Chessboard.rankTitles.map((rankTitle, rankIndex) => (
+            <div key={rankTitle} className={`cell ${this.getAdditionalCssClasses(fileIndex, rankIndex)}`}>
+              {`${`${rankTitle}${fileTitle}`}`}
+            </div>
+          ))
         }
-      </div>
+      </div >
     ));
   }
 
   render() {
     return (
       <div className="chessboard">
-        {ChessboardView.renderEmptyBoard()}
+        {this.renderEmptyBoard()}
       </div>
     );
   }
