6265c1d635786fd99f2f6fa750ac92191b8da722
Dagttatray Kale <way2datta@gmail.com>
2019-12-15 22:16:16 +0530

    Horse implementation

--- chessboard-using-class-based-react/src/lib/PieceLocator.js
+++ chessboard-using-class-based-react/src/lib/PieceLocator.js
@@ -3,6 +3,7 @@ import Bishop from './pieces/Bishop';
 import Queen from './pieces/Queen';
 import King from './pieces/King';
 import Pawn from './pieces/Pawn';
+import Horse from './pieces/Horse';
 
 export class PieceLocator {
   static getPiece(selectedPiece) {
@@ -17,6 +18,8 @@ export class PieceLocator {
         return new King();
       case 'pawn':
         return new Pawn();
+      case 'horse':
+        return new Horse();
       default:
         throw Error('Invalid piece.');
     }
