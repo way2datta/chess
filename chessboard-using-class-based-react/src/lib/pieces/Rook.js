ec509997f15921f892415ce575b4f166181db5a6
Dagttatray Kale <way2datta@gmail.com>
2019-12-18 11:26:51 +0530

    Use get horizontal and vertical moves

--- chessboard-using-class-based-react/src/lib/pieces/Rook.js
+++ chessboard-using-class-based-react/src/lib/pieces/Rook.js
@@ -3,16 +3,8 @@ import { getVerticalMoves } from '../movements/getVerticalMoves';
 
 export default class Rook {
   getPossibleMoves = (fileIndex, rankIndex) => {
-    const horizontalMoves = this.getHorizontalMoves(fileIndex, rankIndex);
-    const verticalMoves = this.getVerticalMoves(fileIndex, rankIndex);
+    const horizontalMoves = getHorizontalMoves(fileIndex, rankIndex);
+    const verticalMoves = getVerticalMoves(fileIndex, rankIndex);
     return horizontalMoves.concat(verticalMoves);
   };
-
-  getHorizontalMoves(fileIndex, rankIndex) {
-    return getHorizontalMoves(fileIndex, rankIndex);
-  }
-
-  getVerticalMoves(fileIndex, rankIndex) {
-    return getVerticalMoves(fileIndex, rankIndex);
-  }
 }
