d2c1ba7350b018d40eeddcb4fd61af03dd0c5952
Dattatray Kale <dkale@internal.tavisca.com>
2020-06-24 11:32:38 +0530

    Board painted :)

--- chess-using-angular9/src/app/board-view/board-view.component.ts
+++ chess-using-angular9/src/app/board-view/board-view.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BoardViewComponent implements OnInit {
   fileTitles: any; // TODO: TS Technical debt 
   rankTitles: any; // TODO: TS Technical debt 
-
+ 
   constructor() { }
 
   ngOnInit(): void {
@@ -16,4 +16,20 @@ export class BoardViewComponent implements OnInit {
     this.fileTitles = ['1', '2', '3', '4', '5', '6', '7', '8'];
   }
 
+  getInitialCellCssClasses(fileIndex, rankIndex) {
+    if (fileIndex % 2 === 0) {
+      return rankIndex % 2 === 0 ? 'white' : 'black';
+    }
+    return rankIndex % 2 === 0 ? 'black' : 'white';
+  }
+
+  getCellClasses(fileIndex, rankIndex) {
+    const initialCellClass = this.getInitialCellCssClasses(fileIndex, rankIndex)
+    let classes = {
+      cell: true,
+      [initialCellClass]: true
+    }
+    console.log({fileIndex, rankIndex,initialCellClass});
+    return classes;
+  }
 }
